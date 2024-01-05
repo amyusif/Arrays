605a7a1cd79d2f19b303c213764de17a4f742fe7
VirtualYMA <98156967+VirtualYMA@users.noreply.github.com>
2024-01-05 01:31:14 +0000

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initializing an array
+
+
+            string[] mates = new string[5];
+            mates[0] = "Yusif";
+            mates[1] = "Adofo";
+
+
+            for (int i = 0; i < mates.Length; i++)
+            {
+                Console.WriteLine(mates[i]);
+            }
+        }
+    }
+}
