12ff380776fb67a1d2b3911b583dc3a6b62a1257
amyusif <am.yusif530@gmail.com>
2024-01-09 06:42:26 +0000

    I made some exercises

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,13 +21,121 @@ namespace Arrays
 
             // Method 2
 
-            string[] mates = { "YUSIF", "ASANTE" };
+            // string[] mates = { "YUSIF", "ASANTE" };
 
             // Looping through an array with FOR LOOP
-            for (int i = 0; i < mates.Length; i++)
+            /* for (int i = 0; i < mates.Length; i++)
+             {
+                 Console.WriteLine(mates[i]);
+             }
+            */
+
+
+
+
+
+            // Create an ARRAY of ints and use loops to find the sum of all integers
+
+            /*  int[] myNums = {2, 4, 6, 8, 10, 12 };
+
+              int sum = 0;
+
+              foreach (int i in myNums)
+              {
+                   sum += i;  
+              }
+              Console.WriteLine(sum);
+            */
+
+
+
+
+            // CREATE AN ARRAY OF NAME STRINGS AND SORT THEM IN ALPHABETICAL ORDER
+
+            /* string[] names = { "Yusif", "Spiff", "Kwame", "Emma" };
+
+              void SortandLoop()
+             {
+                 Array.Sort(names);
+
+                 foreach (string name in names)
+                 {
+                     Console.WriteLine(name);
+                 }
+             }
+
+             SortandLoop();
+            */
+
+
+            // WRITE A PROGRAM TO DISPLAY THE TRANSPOSE OF A MATRIX (2D ARRAY)
+
+            int[,] matrix =
             {
-                Console.WriteLine(mates[i]);
+                {2, 4, 6},
+                {8, 12, 14},
+                {16, 17, 18}
+            };
+
+            int row = matrix.GetLength(0);
+            int col = matrix.GetLength(1);
+
+            // Creating a new matrix for the transpose
+
+           /* int[,] transpose = new int[row, col];
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    transpose[j, i] = matrix[i, j];
+                }
             }
+
+
+            void DisplayMat(int[,] mat)
+            {
+                int rows = mat.GetLength(0);
+                int cols = mat.GetLength(1);
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        Console.Write(transpose[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            DisplayMat(transpose);
+
+            */
+
+
+
+            // ARRAYLIST 
+
+            ArrayList myArrayList = new ArrayList();
+
+
+            myArrayList.Add("Yusif");
+            myArrayList.Add("Mensah");
+            myArrayList.Add(12);
+
+
+            Console.WriteLine( );
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
